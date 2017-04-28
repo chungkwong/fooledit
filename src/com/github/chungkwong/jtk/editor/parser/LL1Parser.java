3d0a08f784160bf763575ac2259867517f4db3fd
Chan Chung Kwong <1m02math@126.com>
2017-04-28 23:56:08 +0800

    Minor changes

--- src/com/github/chungkwong/jtk/editor/parser/LL1Parser.java
+++ src/com/github/chungkwong/jtk/editor/parser/LL1Parser.java
@@ -33,11 +33,24 @@ public class LL1Parser implements Parser{
 		this.terminals=grammar.getTerminals();
 		List<ProductionRule> rules=grammar.getRules();
 		Set<String> nullableSymbols=getNullableSymbols(rules);
-		Map<String,Set<String>> first=getFirst(rules,nullableSymbols);
-		boolean changed=true;
-		while(changed){
-			changed=false;
-
+		MultiMap<String,String> first=getFirst(rules,nullableSymbols);
+		MultiMap<String,String> follow=getFollow(rules,nullableSymbols,first);
+		for(ProductionRule rule:rules){
+			String[] comp=rule.getMember();
+			for(int i=0;i<comp.length;i++){
+				first.get(comp[i]).stream().forEach(t->addItem(t,rule));
+				if(!nullableSymbols.contains(comp[i]))
+					break;
+			}
+			if(nullableSymbols.containsAll(Arrays.asList(comp))){
+				follow.get(rule.getTarget()).stream().forEach(t->addItem(t,rule));
+			}
+		}
+	}
+	private void addItem(String terminal,ProductionRule rule){
+		ProductionRule old=table.put(new Pair<>(rule.getTarget(),terminal),rule);
+		if(old!=null){
+			throw new RuntimeException();
 		}
 	}
 	private Set<String> getNullableSymbols(List<ProductionRule> rules){
@@ -55,18 +68,14 @@ public class LL1Parser implements Parser{
 		}
 		return set;
 	}
-	private Map<String,Set<String>> getFirst(List<ProductionRule> rules,Set<String> nullableSymbols){
-		Map<String,Set<String>> targets=new HashMap<>();
+	private MultiMap<String,String> getFirst(List<ProductionRule> rules,Set<String> nullableSymbols){
+		MultiMap<String,String> targets=new MultiMap<>();
 		boolean changed=true;
 		while(changed){
 			changed=false;
 			for(ProductionRule rule:rules){
 				String target=rule.getTarget();
 				Set<String> found=targets.get(target);
-				if(found==null){
-					found=new HashSet<>();
-					targets.put(target,found);
-				}
 				for(String comp:rule.getMember()){
 					if(terminals.containsKey(comp)){
 						if(!found.contains(comp)){
@@ -75,7 +84,7 @@ public class LL1Parser implements Parser{
 						}
 					}else{
 						Set<String> sub=targets.get(comp);
-						if(sub!=null&&!found.containsAll(sub)){
+						if(!found.containsAll(sub)){
 							found.addAll(sub);
 							changed=true;
 						}
@@ -88,24 +97,84 @@ public class LL1Parser implements Parser{
 		}
 		return targets;
 	}
-	private void getFollow(List<ProductionRule> rules,Set<String> nullableSymbols){
-		
+	private MultiMap<String,String> getFollow(List<ProductionRule> rules,Set<String> nullableSymbols,MultiMap<String,String> first){
+		MultiMap<String,String> targets=new MultiMap<>();
+		for(ProductionRule rule:rules){
+			String[] comp=rule.getMember();
+			for(int i=1;i<comp.length;i++){
+				Set<String> follow=targets.get(comp[i-1]);
+				for(int j=i;j<comp.length;j++){
+					follow.addAll(first.get(comp[j]));
+					if(!nullableSymbols.contains(comp[j]))
+						break;
+				}
+			}
+		}
+		boolean changed=true;
+		while(changed){
+			changed=false;
+			for(ProductionRule rule:rules){
+				String[] comp=rule.getMember();
+				for(int i=comp.length-1;i>=0;i--){
+					changed|=targets.add(rule.getTarget(),targets.get(comp[i]));
+					if(!nullableSymbols.contains(comp[i]))
+						break;
+				}
+			}
+		}
+		return targets;
 	}
 	@Override
 	public Object parse(Iterator<Token> iter){
-		return parse(iter,start);
+		return parse(iter,start).getKey();
 	}
-	private Object parse(Iterator<Token> iter,String symbol){
-		Token forward=iter.next();
+	private Pair<Object,Token> parse(Iterator<Token> iter,String symbol){
+		return parse(iter,symbol,iter.next());
+	}
+	private Pair<Object,Token> parse(Iterator<Token> iter,String symbol,Token forward){
 		if(forward.getType().equals(symbol)){
-			return terminals.get(symbol).apply(forward.getText());
+			return new Pair<>(terminals.get(symbol).apply(forward.getText()),null);
 		}
 		ProductionRule rule=table.get(new Pair<>(symbol,forward.getType()));
 		String[] member=rule.getMember();
 		Object[] comp=new Object[member.length];
 		for(int i=0;i<member.length;i++){
-			comp[i]=parse(iter,member[i]);
+			Pair<Object,Token> tmp=forward==null?parse(iter,symbol):parse(iter,member[i],forward);
+			comp[i]=tmp.getKey();
+			forward=tmp.getValue();
 		}
-		return rule.getAction().apply(comp);
+		return new Pair<>(rule.getAction().apply(comp),forward);
+	}
+}
+class MultiMap<K,V>{
+	private final Map<K,Set<V>> map=new HashMap<>();
+	public boolean add(K key,Set<V> values){
+		Set<V> set=get(key);
+		if(set.containsAll(values)){
+			return false;
+		}else{
+			set.addAll(values);
+			return true;
+		}
+	}
+	public boolean add(K key,V values){
+		Set<V> set=get(key);
+		if(set.contains(values)){
+			return false;
+		}else{
+			set.add(values);
+			return true;
+		}
+	}
+	public Set<V> get(K key){
+		Set<V> set=map.get(key);
+		if(set==null){
+			set=new HashSet<>();
+			map.put(key,set);
+		}
+		return set;
+	}
+	public Map<K,Set<V>> getMap(){
+		return map;
 	}
 }
