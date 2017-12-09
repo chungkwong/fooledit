3b906787ab8fad0a6425c3a4b21d813878a1d661
Chan Chung Kwong <1m02math@126.com>
2017-12-09 23:51:53 +0800

    Minor changes.

--- src/cc/fooledit/api/CoreModule.java
+++ src/cc/fooledit/api/CoreModule.java
@@ -15,12 +15,48 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package cc.fooledit.api;
-
+import cc.fooledit.spi.*;
 /**
  *
  * @author Chan Chung Kwong <1m02math@126.com>
  */
 public class CoreModule{
 	public static final String NAME="core";
-	
+	public static final String CLIP_REGISTRY_NAME="clip";
+	public static final String COMMAND_REGISTRY_NAME="command";
+	public static final String DATA_OBJECT_REGISTRY_NAME="data_object";
+	public static final String DATA_OBJECT_TYPE_REGISTRY_NAME="data_object_type";
+	public static final String EVENT_REGISTRY_NAME="event";
+	public static final String HISTORY_REGISTRY_NAME="history";
+	public static final String MODULE_REGISTRY_NAME="module";
+	public static final String KEYMAP_REGISTRY_NAME="keymap";
+	public static final String LOCALE_REGISTRY_NAME="locale";
+	public static final String MENU_REGISTRY_NAME="menu";
+	public static final RegistryNode<Object> REGISTRY=new SimpleRegistryNode<>(NAME,Registry.ROOT);
+	public static final RegistryNode<Module> CLIP_REGISTRY=new SimpleRegistryNode<>(CLIP_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> COMMAND_REGISTRY=new SimpleRegistryNode<>(COMMAND_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> DATA_OBJECT_REGISTRY=new SimpleRegistryNode<>(DATA_OBJECT_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> DATA_OBJECT_TYPE_REGISTRY=new SimpleRegistryNode<>(DATA_OBJECT_TYPE_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> EVENT_REGISTRY=new SimpleRegistryNode<>(EVENT_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> HISTORY_REGISTRY=new SimpleRegistryNode<>(HISTORY_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> MODULE_REGISTRY=new SimpleRegistryNode<>(MODULE_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> KEYMAP_REGISTRY=new SimpleRegistryNode<>(KEYMAP_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> LOCALE_REGISTRY=new SimpleRegistryNode<>(LOCALE_REGISTRY_NAME,REGISTRY);
+	public static final RegistryNode<Module> MENU_REGISTRY=new SimpleRegistryNode<>(MENU_REGISTRY_NAME,REGISTRY);
+	public static void onLoad(){
+		Registry.ROOT.addChild(REGISTRY);
+		REGISTRY.addChild(CLIP_REGISTRY);
+		REGISTRY.addChild(COMMAND_REGISTRY);
+		REGISTRY.addChild(DATA_OBJECT_REGISTRY);
+		REGISTRY.addChild(DATA_OBJECT_TYPE_REGISTRY);
+		REGISTRY.addChild(EVENT_REGISTRY);
+		REGISTRY.addChild(HISTORY_REGISTRY);
+		REGISTRY.addChild(MODULE_REGISTRY);
+		REGISTRY.addChild(KEYMAP_REGISTRY);
+		REGISTRY.addChild(LOCALE_REGISTRY);
+		REGISTRY.addChild(MENU_REGISTRY);
+	}
+	public static void onUnLoad(){
+
+	}
 }
