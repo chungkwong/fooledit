a91901ebb69c19d6a27663e6f080d46ceb64e515
Chan Chung Kwong <1m02math@126.com>
2018-05-13 17:55:38 +0800

    Used antlr plugin

---
+++ mode.xml/src/main/java/cc/fooledit/editor/text/mode/xml/XMLSupport.java
@@ -0,0 +1,24 @@
+package cc.fooledit.editor.text.mode.xml;
+import java.util.regex.*;
+public class XMLSupport{
+	private static final Pattern XML=Pattern.compile("[Xx][Mm][Ll]");
+	private static final Pattern ENC_NAME=Pattern.compile("[A-Za-z][-A-Za-z0-9._]*");
+	private static final Pattern VERSION_NUMBER=Pattern.compile("1\\.[0-9]+");
+	private static final Pattern PUBLIC_ID=Pattern.compile("[-'()+,./:=?;!*#@$_% \r\na-zA-Z0-9]+");
+	private static final Pattern YES_NO=Pattern.compile("yes|no");
+	public static boolean isXML(String text){
+		return XML.matcher(text).matches();
+	}
+	public static boolean isEncName(String text){
+		return ENC_NAME.matcher(text).matches();
+	}
+	public static boolean isVersionNumber(String text){
+		return VERSION_NUMBER.matcher(text).matches();
+	}
+	public static boolean isPublicId(String text){
+		return PUBLIC_ID.matcher(text).matches();
+	}
+	public static boolean isYesNo(String text){
+		return YES_NO.matcher(text).matches();
+	}
+}
