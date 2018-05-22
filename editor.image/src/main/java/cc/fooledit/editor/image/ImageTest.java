4ebe5dfbe6da70b840bfe42b24698b0cdd2dc14e
Chan Chung Kwong <1m02math@126.com>
2018-05-22 23:58:45 +0800

    JAI is included to support TIFF and other image formats

---
+++ editor.image/src/main/java/cc/fooledit/editor/image/ImageTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018 Chan Chung Kwong <1m02math@126.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cc.fooledit.editor.image;
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
+import javafx.application.*;
+import javafx.embed.swing.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javax.imageio.*;
+/**
+ *
+ * @author Chan Chung Kwong <1m02math@126.com>
+ */
+public class ImageTest extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		System.out.println(Arrays.toString(ImageIO.getReaderFormatNames()));
+		BufferedImage image=ImageIO.read(new File("/home/kwong/projects/tesseract-ocr/phototest.tif"));
+		primaryStage.setScene(new Scene(new BorderPane(new ScrollPane(new ImageView(SwingFXUtils.toFXImage(image,null))))));
+		primaryStage.setMaximized(true);
+		primaryStage.show();
+	}
+	public static void main(String[] args){
+		launch(args);
+	}
+}
